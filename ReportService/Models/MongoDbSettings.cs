7cbdde20a2e9a41b716adddc6d2c0f7a83aecd0f
CheffoPiu <danilo@evolutivos.net>
2025-07-12 12:00:00 -0500

    fix(models): make MongoDbSettings public to fix visibility error

---
+++ ReportService/Models/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+namespace ReportService.Models
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
