eaba5e59797d3177706c4746f49fea6da00c37bb
CheffoPiu <danilo@evolutivos.net>
2025-07-12 12:00:00 -0500

    refactor(program): finalize MongoDB and Swagger configuration

--- ReportService/ReportService.cs
+++ ReportService/ReportService.cs
@@ -1,20 +1,24 @@
 using Microsoft.Extensions.Options;
 using MongoDB.Driver;
+using ReportService.Models;
 
-public class ReportService
+namespace ReportService.Services 
 {
-    private readonly IMongoCollection<Report> _reportCollection;
-
-    public ReportService(IOptions<MongoDbSettings> settings)
+    public class ReportMongoService
     {
-        var client = new MongoClient(settings.Value.ConnectionString);
-        var database = client.GetDatabase(settings.Value.DatabaseName);
-        _reportCollection = database.GetCollection<Report>("reports");
+        private readonly IMongoCollection<Report> _reportCollection;
+
+        public ReportMongoService(IOptions<MongoDbSettings> settings)
+        {
+            var client = new MongoClient(settings.Value.ConnectionString);
+            var database = client.GetDatabase(settings.Value.DatabaseName);
+            _reportCollection = database.GetCollection<Report>("reports");
+        }
+
+        public async Task CreateReport(Report report) =>
+            await _reportCollection.InsertOneAsync(report);
+
+        public async Task<List<Report>> GetReports() =>
+            await _reportCollection.Find(_ => true).ToListAsync();
     }
-
-    public async Task CreateReport(Report report) =>
-        await _reportCollection.InsertOneAsync(report);
-
-    public async Task<List<Report>> GetReports() =>
-        await _reportCollection.Find(_ => true).ToListAsync();
 }
